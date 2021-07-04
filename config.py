ce6f73cb97355c4625cd0efafbe0969fe66e5251
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-04 18:46:49 +0530

    ❣️

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
       # Bot Fater 
       MT_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       # MT MyTelegramOrg Bot
-      APP_ID = int(os.environ.get("APP_ID", 12345))
+      API_ID = int(os.environ.get("APP_ID", 12345))
       # MT MyTelegramOrg Bot
       API_HASH = os.environ.get("API_HASH")
