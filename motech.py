3b1745e655d24a918d9f424f17c4388998d7e35b
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-04 20:55:38 +0530

    ❣️

--- motech.py
+++ motech.py
@@ -9,10 +9,11 @@ Motechyt = Client(
             api_hash = os.environ["API_HASH"]
 )
 
+# start 
 @Motechyt.on_message(filters.private & filters.command("start"))
 async def start(motech, update):
     text = f"""
-<b> 👋Hello {update.from_user.mention}
+<b> 👋Hello {update.from_user.mention}</b>
 
 <b>I CAN GET ANY PUBLIC AND PRIVATE CHANNEL ID
 
@@ -29,6 +30,7 @@ CLICK /INFO GET YOUR TELEGRAM INFO </b>
         reply_markup=reply_markup
   )
 
+# Button Controler 
 MT_START = InlineKeyboardMarkup(
      [[
         InlineKeyboardButton("🗣️Group", url=f"t.me/mo_tech_group"),
@@ -37,6 +39,7 @@ MT_START = InlineKeyboardMarkup(
      ]]
    )
 
+# telegram information from motech.py
 @Motechyt.on_message(filters.private & filters.command("info"))
 async def info(motech, update):
     if update.from_user.last_name:
@@ -74,5 +77,41 @@ async def id(motech, update):
         disable_web_page_preview=True,
         reply_markup=reply_markup
     )
+
+# Forwarded message id from motech.py
+@Motechyt.on_message(filters.private & filters.forwarded)
+async def forwarded(motech, msg):
+    if msg.forward_from:
+        text = "🤫Forward Information🤫\n\n"
+        if msg.forward_from["is_bot"]:
+            text += "<b>🤖Bot</b>\n\n"
+        else:
+            text += "<b>👤User</b>\n\n"
+        text += f'\n👨‍💼Name{msg.forward_from["first_name"]} \n'
+        if msg.forward_from["username"]:
+            text += f'🔗 UserName : @{msg.forward_from["username"]} \nID : `{msg.forward_from["id"]}`'
+        else:
+            text += f'🆔 ID : `{msg.forward_from["id"]}`'
+        await msg.reply(text, quote=True)
+    else:
+        hidden = msg.forward_sender_name
+        if hidden:
+            await msg.reply(
+                f"Forward detected but unfortunately, {hidden} has enabled forwarding privacy, so I can't get their id",
+                quote=True,
+            )
+        else:
+            text = f"🤫Forward Information🤫. \n\n"
+            if msg.forward_from_chat["type"] == "channel":
+                text += "<b>📢 Channel</b>\n\n"
+            if msg.forward_from_chat["type"] == "supergroup":
+                text += "<b>🗣️ Group</b>\n\n"
+            text += f'📃 Name\n{msg.forward_from_chat["title"]} \n\n'
+            if msg.forward_from_chat["username"]:
+                text += f'<b>➡️ From</b> : @{msg.forward_from_chat["username"]} \n\n'
+                text += f'<b>🆔 ID</b> : `{msg.forward_from_chat["id"]}`\n\n'
+            else:
+                text += f'<b>🆔 ID</b> `{msg.forward_from_chat["id"]}`\n\n'
+            await msg.reply(text, quote=True)
 
 Motechyt.run()
