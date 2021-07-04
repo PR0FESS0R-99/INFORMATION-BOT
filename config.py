e35b798f811ab91dec26331844f1e5b3a6918095
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-07-04 18:43:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 
       # Bot Fater 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      MT_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       # MT MyTelegramOrg Bot
       APP_ID = int(os.environ.get("APP_ID", 12345))
       # MT MyTelegramOrg Bot
