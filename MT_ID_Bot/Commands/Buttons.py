69efb1c9e5b52249e8b39ddf80dad03e8da416ca
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 14:21:24 +0530

    New Update..

---
+++ MT_ID_Bot/Commands/Buttons.py
@@ -0,0 +1,26 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+START_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("📢 𝐁𝐨𝐭 𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url=f"t.me/mo_tech_yt"),
+       InlineKeyboardButton("𝐎𝐩𝐞𝐧 𝐒𝐨𝐮𝐫𝐜𝐞 💫", url=f"https://github.com/PR0FESS0R-99/ID-Bot")
+       ],[
+       InlineKeyboardButton("⬇️ 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ⬇️", callback_data="help")
+       ]]
+       )
+
+HELP_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐝", callback_data="id"),
+       InlineKeyboardButton("𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐧𝐟𝐨", callback_data="info")
+       ],[
+       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close"),
+       InlineKeyboardButton("🤠 𝐀𝐛𝐨𝐮𝐭", callback_data="about")
+       ]]
+       )
+
+ABOUT_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤", callback_data="help"),
+       InlineKeyboardButton("🏠 𝐇𝐨𝐦𝐞", callback_data="start"),
+       InlineKeyboardButton("⬇️ 𝐂𝐥𝐨𝐬𝐞", callback_data="close")
+       ]]
+       )
