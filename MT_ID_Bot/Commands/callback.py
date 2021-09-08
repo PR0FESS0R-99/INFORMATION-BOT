0b7125ddcf41b640eb0c56ed6e34aeef55391d88
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 22:48:35 +0530

    New Update....

--- MT_ID_Bot/Commands/callback.py
+++ MT_ID_Bot/Commands/callback.py
@@ -4,6 +4,7 @@ from MT_ID_Bot.Translation import Translation
 from MT_ID_Bot.Config import Config
 from MT_ID_Bot.Commands.Buttons import START_BUTTON, HELP_BUTTON, ABOUT_BUTTON
 from MT_ID_Bot.Commands.Commands import developer, co_developer, source, mt_chat, mt_bot
+from MT_ID_Bot.Modules.Buttons import ID_BUTTONS, INFO_BUTTONS
 
 UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
 
@@ -36,6 +37,26 @@ async def cb_handler(client, query):
         await query.message.edit_text(
             Translation.ABOUT_MSG.format(BOT_USERNAME, developer, co_developer, mt_chat, mt_bot, source),
             reply_markup=ABOUT_BUTTON,
+            disable_web_page_preview=True
+        )
+        return
+
+    if query.data == "id":
+        await query.answer()
+
+        await query.message.edit_text(
+            Translation.ID_TEXT.format(query.from_user.id),
+            reply_markup=ID_BUTTONS,
+            disable_web_page_preview=True
+        )
+        return
+
+    elif query.data == "info":
+        await query.answer()
+
+        await query.message.edit_text(
+            Translation.INFO_TEXT.format(query.from_user.first_name, query.from_user.last_name, query.from_user.username, query.from_user.id, query.from_user.mention, query.from_user.dc_id, query.from_user.language_code, query.from_user.status),
+            reply_markup=INFO_BUTTONS,
             disable_web_page_preview=True
         )
         return
