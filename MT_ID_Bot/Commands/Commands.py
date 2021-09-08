9f17ac5eef24147b4dbb74c719a8d40007909115
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 11:55:45 +0530

    New Update....

--- MT_ID_Bot/Commands/Commands.py
+++ MT_ID_Bot/Commands/Commands.py
@@ -1,1 +1,102 @@
+"""
+from pyrogram import filters
+from pyrogram import Client as MT_ID_Bot
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from MT_ID_Bot.Translation import Translation
+from MT_ID_Bot.Config import Config
+from MT_ID_Bot.Commands.Buttons import START_BUTTON
 
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
+
+@MT_ID_Bot.on_message(filters.private & filters.command("start"))
+async def start_handler(motech, update):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await update.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await update.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+    reply_markup =  START_BUTTON
+    await update.reply_text(
+        text=Translation.START_MSG.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )
+
+
+@MT_ID_Bot.on_message(filters.private & filters.command("about"))
+async def help_handler(motech, update):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await update.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await update.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+    reply_markup =  HELP_BUTTON
+    await update.reply_text(
+        text=Translation.HELP_MSG,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )
+
+@MT_ID_Bot.on_message(filters.private & filters.command("start"))
+async def about_handler(motech, update):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await update.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await update.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+    reply_markup =  ABOUT_BUTTON
+    await update.reply_text(
+        text=Translation.ABOUT_MSG,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+  )"""
