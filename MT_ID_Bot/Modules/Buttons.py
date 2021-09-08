ad780d7a1ca325f853423b5f37b130715295ae27
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 20:49:04 +0530

    New Update....

--- MT_ID_Bot/Modules/Buttons.py
+++ MT_ID_Bot/Modules/Buttons.py
@@ -1,11 +1,15 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 ID_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 Join My Updates 📢", url=f"t.me/mo_tech_yt")
+       InlineKeyboardButton("↗️ 𝐆𝐞𝐭 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ↗️", callback_data="info")
+       ],[
+       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐇𝐞𝐥𝐩 🔙", callback_data="help")
        ]]
        )
 INFO_BUTTON = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 Join My Updates 📢", url=f"t.me/mo_tech_yt")
+       InlineKeyboardButton("↗️ 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐃 ↗️", callback_data="id")
+       ],[
+       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐇𝐞𝐥𝐩 🔙", callback_data="help")
        ]]
        )
 ID_BUTTONS = InlineKeyboardMarkup( [[
@@ -15,6 +19,8 @@ ID_BUTTONS = InlineKeyboardMarkup( [[
        ]]
        )
 INFO_BUTTONS = InlineKeyboardMarkup( [[
-       InlineKeyboardButton("📢 Join My Updates 📢", url=f"t.me/mo_tech_yt")
+       InlineKeyboardButton("↗️ 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐈𝐃 ↗️", callback_data="id")
+       ],[
+       InlineKeyboardButton("🔙 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐇𝐞𝐥𝐩 🔙", callback_data="help")
        ]]
        )
