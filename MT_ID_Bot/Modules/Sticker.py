0e17f08ac75b8529785f380ef98d96d5cd8140d5
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 15:52:15 +0530

    Sticker Id Added

---
+++ MT_ID_Bot/Modules/Sticker.py
@@ -0,0 +1,52 @@
+from pyrogram import filters
+from pyrogram import Client as MT_ID_Bot
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from MT_ID_Bot.Translation import Translation
+from MT_ID_Bot.Config import Config
+from MT_ID_Bot.Commands.Buttons import START_BUTTON, HELP_BUTTON, ABOUT_BUTTON
+
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
+from MT_ID_Bot.Translation import Translation
+from MT_ID_Bot.Config import Config
+
+UPDATE_CHANNEL=Config.UPDATE_CHANNEL # Update Channel Forces Subscribe
+BOT_USERNAME=Config.BOT_USERNAME # ReStart Option 
+JOIN=Translation.JOIN_TEXT # Button Text (Update Channel)
+TRY=Translation.TRY_TEXT # Button Text (Update Channel)
+SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
+
+# ADDED STICKER ID GETTING. COPYRIGHT UNDER AND RE-GENERATED AND
+# MODED BY @MR-JINN-OF-TG AND TO @PR0FESS0R-99
+
+@MT_ID_Bot.on_message(filters.private & ~filters.forwarded & ~filters.command(["start", "help", "about", "info", "information", "json", "source", "id"]))
+async def stickers(motech, msg):
+    update_channel = UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await motech.get_chat_member(update_channel, msg.chat.id)
+            if user.status == "kicked out":
+               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
+            await msg.reply_text(
+                text=f"<b>{SUB_TEXT}</b>",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
+                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
+              ])
+            )
+            return
+        except Exception:
+            await msg.reply_text(f"@{UPDATE_CHANNEL}")
+            return  
+
+    if msg.sticker:
+        await msg.reply(f"𝐘𝐨𝐮𝐫 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐈𝐃 𝐢𝐬`{msg.sticker.file_id}`", quote=True)
+    else:
+        await msg.reply(Translation.ID_TEXT.format(msg.from_user.id))
