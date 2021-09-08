855128ba596db494d3710cb7a06fe91df77f8a8d
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-09-08 19:22:39 +0530

    Update Buttons.py

--- MT_ID_Bot/Modules/Buttons.py
+++ MT_ID_Bot/Modules/Buttons.py
@@ -1,1 +1,10 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
+ID_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("📢 Join My Updates 📢", url=f"t.me/mo_tech_yt"),
+       ]]
+       )
+INFO_BUTTON = InlineKeyboardMarkup( [[
+       InlineKeyboardButton("📢 Join My Updates 📢", url=f"t.me/mo_tech_yt"),
+       ]]
+       )
