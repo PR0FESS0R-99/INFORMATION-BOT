4288a396f1610b41a61f00e297107e92a14626d1
PR0FESS0R-99 <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-10-03 17:45:53 +0530

    Added Start Support

---
+++ start.sh
@@ -0,0 +1,7 @@
+6 lines (6 sloc)  177 Bytes
+echo "Cloning Repo...."
+git clone https://github.com/PR0FESS0R-99/ID-Bot /ID-Bot
+cd /ID-Bot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 motech.py
