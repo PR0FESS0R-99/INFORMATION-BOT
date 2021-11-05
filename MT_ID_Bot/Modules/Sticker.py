ec9ed3b81922f5ac5f66f0b2d57da021a1635712
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-11-05 05:06:25 -0700

    bug Fix<=>

--- MT_ID_Bot/Modules/Sticker.py
+++ MT_ID_Bot/Modules/Sticker.py
@@ -14,28 +14,6 @@ SUB_TEXT=Translation.FSUB_TEXT # FSUB Information Text
 # ADDED STICKER ID GETTING. COPYRIGHT UNDER AND RE-GENERATED AND
 # MODED BY @MR-JINN-OF-TG AND TO @PR0FESS0R-99
 
-@MT_ID_Bot.on_message(filters.private & ~filters.forwarded & ~filters.command(["start", "help", "about", "info", "information", "json", "source", "id"]))
-async def stickers(motech, msg):
-    update_channel = UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await motech.get_chat_member(update_channel, msg.chat.id)
-            if user.status == "kicked out":
-               await update.reply_text("😔 Sorry Dude, You are **🅱︎🅰︎🅽︎🅽︎🅴︎🅳︎ 😜**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{Channel User Name} To Use Me") From Motech.py
-            await msg.reply_text(
-                text=f"<b>{SUB_TEXT}</b>",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text=f"{JOIN}", url=f"t.me/{UPDATE_CHANNEL}")],
-                    [ InlineKeyboardButton(text=f"{TRY}", url=f"https://t.me/{BOT_USERNAME}?start=try")]
-              ])
-            )
-            return
-        except Exception:
-            await msg.reply_text(f"@{UPDATE_CHANNEL}")
-            return  
 @MT_ID_Bot.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
