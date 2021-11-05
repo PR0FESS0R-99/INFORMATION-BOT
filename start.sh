afa40ac0ae7947d39fe25d28a8a857ad2de8f199
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2021-11-05 14:18:56 +0530

    🌟🌟

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/PR0FESS0R-99/ID-Bot-V1.git /ID-Bot-V1
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /ID-Bot-V1
+fi
+cd /ID-Bot-V1
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 code.py
